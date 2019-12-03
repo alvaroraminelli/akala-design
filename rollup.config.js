83acfe894415ba7e0af7b812aca1722a474943d7
Alvaro Raminelli <alvaro.raminelli@gmail.com>
2019-12-03 14:42:54 -0800

    fix: improving config

--- rollup.config.js
+++ rollup.config.js
@@ -15,40 +15,40 @@ export default {
     { file: pkg.main, format: 'cjs', sourcemap: true }
   ],
   plugins: [
-    replace({
-      "process.env.NODE_ENV": JSON.stringify(NODE_ENV)
-    }),
+  //   replace({
+  //     "process.env.NODE_ENV": JSON.stringify(NODE_ENV)
+  //   }),
     babel({
       plugins: [
         "@babel/plugin-proposal-class-properties",
         "@babel/plugin-proposal-export-default-from"
       ],
       exclude: "node_modules/**"
-    }),
-    resolve(),
-    commonjs({
-      include: 'node_modules/**',
-      // left-hand side can be an absolute path, a path
-      // relative to the current directory, or the name
-      // of a module in node_modules
-      namedExports: {
-        'node_modules/react/index.js': [
-          'cloneElement',
-          'createContext',
-          'Component',
-          'createElement',
-          'isValidElement',
-          'Children',
-          'createRef'
-        ],
-        'node_modules/react-dom/index.js': ['render', 'hydrate'],
-        'node_modules/react-is/index.js': [
-          'isElement',
-          'isValidElementType',
-          'ForwardRef'
-        ]
-      }
     })
+  //   resolve(),
+  //   commonjs({
+  //     include: 'node_modules/**',
+  //     // left-hand side can be an absolute path, a path
+  //     // relative to the current directory, or the name
+  //     // of a module in node_modules
+  //     namedExports: {
+  //       'node_modules/react/index.js': [
+  //         'cloneElement',
+  //         'createContext',
+  //         'Component',
+  //         'createElement',
+  //         'isValidElement',
+  //         'Children',
+  //         'createRef'
+  //       ],
+  //       'node_modules/react-dom/index.js': ['render', 'hydrate'],
+  //       'node_modules/react-is/index.js': [
+  //         'isElement',
+  //         'isValidElementType',
+  //         'ForwardRef'
+  //       ]
+  //     }
+  //   })
   ]
 }
 
